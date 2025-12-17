bbfeb3965a77e067ff23211c5a76cb6617b2bacf
Verity Allison <verityalls@gmail.com>
2025-12-17 16:46:34 +0000

    add fruit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,14 @@
 fruits = ["🍌", "🍏", "🍊", "🍌", "🍌", "🍏", "🍊", "🍏", "🍊", "🍊", "🍏", "🍌"]; 
 
-const shelf = { 
-    "🍌": "",
-    "🍊": "",
-    "🍏": ""
-};
-
-const whichShelf = {
-    "🍌": "#banana-shelf",
-    "🍊": "#orange-shelf",
-    "🍏": "#apple-shelf"
-};
-
-for (const fruit of fruits) {
-    if (shelf.hasOwnProperty(fruit)) {
-        shelf[fruit] += fruit;
-    } else {
-        console.log(`${fruit} goes in the bin 🗑️`);
-    }
-}
-
-for (const [emoji, selector] of Object.entries(whichShelf)) {
-    document.querySelector(selector).textContent = shelf[emoji];
-}
+for (const fruit of fruits) { 
+    if (fruit === "🍌") { 
+        document.querySelector("#banana-shelf").textContent += fruit; 
+    } 
+        else if (fruit === "🍊") { 
+            document.querySelector("#orange-shelf").textContent += fruit; 
+        } 
+        else if (fruit === "🍏") { 
+            document.querySelector("#apple-shelf").textContent += fruit; 
+        } 
+            else { console.log(`${fruit} goes in the bin 🗑️`); 
+        } }
