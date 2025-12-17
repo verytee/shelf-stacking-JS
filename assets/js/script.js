c7dac63ddcf70e7d9258501759c999599853311c
Verity Allison <verityalls@gmail.com>
2025-12-17 16:32:01 +0000

    stacked fruit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
-fruits = ["🍌", "🍏", "🍊", "🍌", "🍌", "🍏", "🍊", "🍏", "🍊", "🍊", "🍏", "🍌"];
+fruits = ["🍌", "🍏", "🍊", "🍌", "🍌", "🍏", "🍊", "🍏", "🍊", "🍊", "🍏", "🍌"]; 
 
+const shelf = { 
+    "🍌": "",
+    "🍊": "",
+    "🍏": ""
+};
 
+const whichShelf = {
+    "🍌": "#banana-shelf",
+    "🍊": "#orange-shelf",
+    "🍏": "#apple-shelf"
+};
 
+for (const fruit of fruits) {
+    if (shelf.hasOwnProperty(fruit)) {
+        shelf[fruit] += fruit;
+    } else {
+        console.log(`${fruit} goes in the bin 🗑️`);
+    }
+}
+
+for (const [emoji, selector] of Object.entries(whichShelf)) {
+    document.querySelector(selector).textContent = shelf[emoji];
+}
